99adc2e8f0d076a130d72260531a950421525606
XMMR12 <31443996+XMMR12@users.noreply.github.com>
2020-10-15 19:32:17 +0200

    Facebook js script for adding by mutual friends
    This script is used to add the friends that has dynamic link between you and your current friends
    using pureJS
    
    link to youtube :https://youtu.be/t_uhFIg3MMU
    
    updated on: october 2020

---
+++ Facebook add by mutual friends Script.js
@@ -0,0 +1,13 @@
+usr_n=prompt("how many mutual friends between u and that person?");
+usr_int=parseInt(usr_n);
+c="bp9cbjyn ow4ym5g4 auili1gw rq0escxv j83agx80 btwxx1t3 buofh1pr g5gj957u i1fnvgqd mysgfdmx cxmmr5t8 hcukyx3x hddg9phg tgvbjcpo hpfvmrgz rz4wbd8a a8nywdso l9j0dhe7 du4w35lb rj1gh0hx pybr56ya f10w8fjw lhclo0ds";
+v=document.getElementsByClassName(c);
+for(var n=0;n<=v.length;n++){if(v[n].children[0].children[0].children[1]){m_f=v[n].children[0].children[0].children[1].textContent.trimStart().split(" ")[0];
+if (parseInt(usr_n)<=parseInt(m_f)){v[n].children[1].children[0].children[0].children[0].children[0].click();
+}
+}
+}
+
+
+
+//document.location=web.facebook.com/friends/
